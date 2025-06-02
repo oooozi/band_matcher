8d2d7141c1fe66e59ad0cd5dbcaf18505b04c242
oooozi <ojw042204@gmail.com>
2025-06-02 13:26:13 +0900

    flask-cors 추가

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,11 +2,13 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from .app_db import db, migrate
+from flask_cors import CORS
 
 def create_app():
     load_dotenv()
 
     app = Flask(__name__)
+    CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
